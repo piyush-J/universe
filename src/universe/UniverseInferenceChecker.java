692fcdd438a489a5a6befa24fbe5be9f3c0af924
Piyush Jha <piyush.jha@uwaterloo.ca>
2021-12-16 05:26:44 -0500

    Cleanup src

--- src/universe/UniverseInferenceChecker.java
+++ src/universe/UniverseInferenceChecker.java
@@ -1,33 +1,53 @@
 package universe;
 
-import javax.annotation.processing.SupportedOptions;
-
-import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
-import org.checkerframework.framework.source.SupportedLintOptions;
-
+import checkers.inference.BaseInferenceRealTypeFactory;
 import checkers.inference.BaseInferrableChecker;
 import checkers.inference.InferenceAnnotatedTypeFactory;
 import checkers.inference.InferenceChecker;
 import checkers.inference.InferenceVisitor;
 import checkers.inference.InferrableChecker;
 import checkers.inference.SlotManager;
+import universe.qual.Any;
+import universe.qual.Bottom;
+import universe.qual.Lost;
+import universe.qual.Peer;
+import universe.qual.Rep;
+import universe.qual.Self;
 import checkers.inference.model.ConstraintManager;
+import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
+import org.checkerframework.dataflow.qual.Pure;
+import org.checkerframework.framework.source.SupportedLintOptions;
+import org.checkerframework.javacutil.AnnotationBuilder;
+
+import javax.lang.model.element.AnnotationMirror;
+
 
 /**
  * The main checker class for the Generic Universe Types checker.
  *
  * @author wmdietl
  */
-// Keep these synchronized with the superclass.
-@SupportedOptions( { "warn" } )
-@SupportedLintOptions({"allowLost", "checkOaM", "checkStrictPurity"})
+@SupportedLintOptions({"checkOaM", "checkStrictPurity"})
 public class UniverseInferenceChecker extends BaseInferrableChecker {
 
+    public static AnnotationMirror ANY, PEER, REP, LOST, SELF, BOTTOM, PURE;
+
     @Override
-    public BaseAnnotatedTypeFactory createRealTypeFactory() {
-        // Return the UniverseInferenceAnnotatedTypeFactory so that it can carry
-        // UniverseInferenceVariableAnnotator
-        return new UniverseInferenceAnnotatedTypeFactory(this);
+    public void initChecker() {
+        super.initChecker();
+        ANY = AnnotationBuilder.fromClass(getElementUtils(), Any.class);
+        PEER = AnnotationBuilder.fromClass(getElementUtils(), Peer.class);
+        REP = AnnotationBuilder.fromClass(getElementUtils(), Rep.class);
+        LOST = AnnotationBuilder.fromClass(getElementUtils(), Lost.class);
+        SELF = AnnotationBuilder.fromClass(getElementUtils(), Self.class);
+        BOTTOM = AnnotationBuilder.fromClass(getElementUtils(), Bottom.class);
+        PURE = AnnotationBuilder.fromClass(getElementUtils(), Pure.class);
+
+    }
+
+    @Override
+    public BaseInferenceRealTypeFactory createRealTypeFactory(boolean infer) {
+        return new UniverseAnnotatedTypeFactory(this, infer);
     }
 
     @Override
@@ -41,19 +61,18 @@ public class UniverseInferenceChecker extends BaseInferrableChecker {
         return true;
     }
 
-    /*    @Override
-    public boolean withExistentialVariables() {
-        return false;
-    }*/
-
     @Override
     public InferenceAnnotatedTypeFactory createInferenceATF(
             InferenceChecker inferenceChecker, InferrableChecker realChecker,
             BaseAnnotatedTypeFactory realTypeFactory, SlotManager slotManager,
             ConstraintManager constraintManager) {
         return new UniverseInferenceAnnotatedTypeFactory(inferenceChecker,
-        	withCombineConstraints(), realTypeFactory, realChecker,
+                withCombineConstraints(), realTypeFactory, realChecker,
                 slotManager, constraintManager);
     }
 
+    @Override
+    public boolean isInsertMainModOfLocalVar() {
+        return true;
+    }
 }
---
+++ src/universe/UniverseViewpointAdapter.java
@@ -0,0 +1,59 @@
+package universe;
+
+import static universe.UniverseInferenceChecker.ANY;
+import static universe.UniverseInferenceChecker.BOTTOM;
+import static universe.UniverseInferenceChecker.LOST;
+import static universe.UniverseInferenceChecker.PEER;
+import static universe.UniverseInferenceChecker.REP;
+import static universe.UniverseInferenceChecker.SELF;
+
+import javax.lang.model.element.AnnotationMirror;
+
+import org.checkerframework.framework.type.AbstractViewpointAdapter;
+import org.checkerframework.framework.type.AnnotatedTypeFactory;
+import org.checkerframework.framework.type.AnnotatedTypeMirror;
+import org.checkerframework.javacutil.AnnotationUtils;
+
+public class UniverseViewpointAdapter extends AbstractViewpointAdapter {
+
+    public UniverseViewpointAdapter(AnnotatedTypeFactory atypeFactory) {
+        super(atypeFactory);
+    }
+
+    @Override
+    protected AnnotationMirror extractAnnotationMirror(AnnotatedTypeMirror atm) {
+        assert atm != null;
+        return atm.getAnnotationInHierarchy(ANY);
+    }
+
+    @Override
+    protected AnnotationMirror combineAnnotationWithAnnotation(AnnotationMirror receiverAnnotation,
+            AnnotationMirror declaredAnnotation) {
+        assert receiverAnnotation != null;
+        assert declaredAnnotation != null;
+
+        if (AnnotationUtils.areSame(receiverAnnotation, SELF)) {
+            return declaredAnnotation;
+        } else if (AnnotationUtils.areSame(declaredAnnotation, BOTTOM)) {
+            return BOTTOM;
+        } else if (AnnotationUtils.areSame(declaredAnnotation, ANY)) {
+            return ANY;
+        } else if (AnnotationUtils.areSame(declaredAnnotation, SELF)) {
+        	return receiverAnnotation;
+        } else if (AnnotationUtils.areSame(receiverAnnotation, BOTTOM)) {
+            // If receiver is bottom, has no ownership information. Any member
+            // of it from the viewpoint of self is any, except when declared
+            // type is bottom.
+            return ANY;
+        } else if (AnnotationUtils.areSame(declaredAnnotation, LOST)) {
+            return LOST;
+        } else if (AnnotationUtils.areSame(declaredAnnotation, PEER)) {
+            if (AnnotationUtils.areSame(receiverAnnotation, PEER)) {
+                return PEER;
+            } else if (AnnotationUtils.areSame(receiverAnnotation, REP)) {
+                return REP;
+            }
+        }
+        return LOST;
+    }
+}
